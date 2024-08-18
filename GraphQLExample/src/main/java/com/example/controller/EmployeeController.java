4fb33ef96c8dd27f7c59a3e28e54cfebfb41ac53
vaibhu9 <vaibhavshejol9@gmail.com>
2024-08-18 23:13:58 +0530

    Endpoint created of GraphQL

--- GraphQLExample/src/main/java/com/example/controller/EmployeeController.java
+++ GraphQLExample/src/main/java/com/example/controller/EmployeeController.java
@@ -2,6 +2,8 @@ package com.example.controller;
 
 import com.example.entity.Employee;
 import com.example.service.EmployeeService;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,8 +23,9 @@ public class EmployeeController {
         return employeeService.createEmployee(employee);
     }
 
-    @GetMapping("/{id}")
-    public Employee getEmployee(@PathVariable Long id) {
+//    @GetMapping("/{id}")
+    @QueryMapping("getEmployee")
+    public Employee getEmployee(/*@PathVariable*/ @Argument Long id) {
         return employeeService.getEmployee(id);
     }
 
