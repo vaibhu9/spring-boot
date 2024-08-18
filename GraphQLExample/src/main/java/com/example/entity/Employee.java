b86e3b7a92b67698ce54d7b7a45d26c83d2944d3
vaibhu9 <vaibhavshejol9@gmail.com>
2024-08-18 22:18:26 +0530

    Entity created

---
+++ GraphQLExample/src/main/java/com/example/entity/Employee.java
@@ -0,0 +1,25 @@
+package com.example.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "contact")
+    private String contact;
+
+    @Column(name = "salary")
+    private double salary;
+}
