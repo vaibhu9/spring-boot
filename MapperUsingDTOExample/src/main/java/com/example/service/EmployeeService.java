5e0d4f1db772056f57abf3dd628386189fbe0382
vaibhu9 <vaibhavshejol9@gmail.com>
2024-08-20 21:28:39 +0530

    Employee service interface created

---
+++ MapperUsingDTOExample/src/main/java/com/example/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.example.service;
+
+import com.example.request.EmployeeRequest;
+import com.example.response.EmployeeRespose;
+
+import java.util.List;
+
+public interface EmployeeService {
+    EmployeeRespose createEmployee(EmployeeRequest employeeRequest);
+    EmployeeRespose getEmployee(Long id);
+    List<EmployeeRespose> getAllEmployees();
+    EmployeeRespose updateEmployee(Long id, EmployeeRequest employeeRequest);
+    Void deleteEmployee(Long id);
+}
