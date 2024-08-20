a7e63d59073df8f4cde71a50bfbe97c13ca349a0
vaibhu9 <vaibhavshejol9@gmail.com>
2024-08-20 22:30:51 +0530

    EmployeeController created

---
+++ MapperUsingDTOExample/src/main/java/com/example/controller/EmployeeController.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import com.example.service.EmployeeService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/employee")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+    
+}
