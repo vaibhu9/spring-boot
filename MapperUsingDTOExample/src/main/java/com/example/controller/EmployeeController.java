0d3b3d00b32514b90a3c73e65153902c400a9361
vaibhu9 <vaibhavshejol9@gmail.com>
2024-08-20 22:33:50 +0530

    createEmployee API created

--- MapperUsingDTOExample/src/main/java/com/example/controller/EmployeeController.java
+++ MapperUsingDTOExample/src/main/java/com/example/controller/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.example.controller;
 
+import com.example.request.EmployeeRequest;
+import com.example.response.EmployeeRespose;
 import com.example.service.EmployeeService;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +16,9 @@ public class EmployeeController {
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
-    
+
+    @PostMapping
+    EmployeeRespose createEmployee(EmployeeRequest employeeRequest) {
+        return employeeService.createEmployee(employeeRequest);
+    }
 }
